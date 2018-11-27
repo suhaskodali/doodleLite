7a4575b4f7a70ea6cbef7f7e52d1a27dd73ab30e
Justin Walters <waltersjustin89@gmail.com>
2018-11-27 09:51:26 -0500

    sessions add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get  '/home', to: 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/signup',  to: 'users#new'
+  get  '/home',     to: 'static_pages#home'
+  get  '/help',     to: 'static_pages#help'
+  get  '/signup',   to: 'users#new'
+  get  '/login',    to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to:'sessions#destroy'
   root 'static_pages#home'
   resources :polls
   resources :users
